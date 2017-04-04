dc53c06bc71b69f39750eb48164e93b4b62af196
Yuichi Saito <munisystem@gmail.com>
2017-04-04 15:06:01 +0900

    Fix don't execute npm run local-run

--- lambdaDriver.js
+++ lambdaDriver.js
@@ -1,16 +1,17 @@
 'use strict';
 
-var index = require('./dist/index')
+var index = require('./lib/index');
 
-var event = {}
+var event = {};
 
-var context = {}
+var context = {};
 
 var callback = function (err, result) {
   if (err) {
-    console.log(err.message)
+    console.log(err.message);
   } else {
-    console.log(result)
+    console.log(result);
   }
 }
 
+index.handler(event, context, callback);
