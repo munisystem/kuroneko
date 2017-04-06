3e2acfdeca01ec2e6e9caf747832b8ae3fa1f9cb
Yuichi Saito <munisystem@gmail.com>
2017-04-06 11:08:58 +0900

    Support bulk insert

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@
 const aws = require('aws-sdk');
 const rds = new aws.RDS();
 const DBInstanceIdentifier = process.env.AWS_DB_INSTANCE_IDENTIFIER;
+const esIndex = 'psql_query_log'
 
 const elasticsearch = require('elasticsearch');
 const client = new elasticsearch.Client({
-  host: 'localhost:9200',
-  log: 'trace'
+  host: 'localhost:9200'
 });
 
 const plpr = require('plpr');
@@ -15,19 +15,21 @@ const plpr = require('plpr');
 exports.handler = (event, context, callback) => {
   downloadLogFile().then(data => {
     const logs = plpr(data);
+    console.log('Insert data length: ' + logs.length);
+    var body = [];
+    const description = JSON.stringify({index: {_index: esIndex, _type: DBInstanceIdentifier}});
     logs.forEach((element, index, array) => {
-      client.index({
-        index: 'rds_log',
-        type: DBInstanceIdentifier,
-        body: {
-          timestamp: Date.parse(element.timestamp),
-          duration: element.duration,
-          query: element.query
-        },
-      });
+      body.push(description);
+      body.push(JSON.stringify({timestamp: Date.parse(element.timestamp), duration: element.duration, query: element.query}));
+    });
+
+    client.bulk({
+      body: body
+    }, (error, response) => {
+      if (error) return callback(error, error);
+      else return callback(null, 'success');
     });
   });
-  return callback(null, 'success');
 }
 
 function getLogFiles() {
@@ -52,6 +54,7 @@ function downloadLogFile() {
     };
 
     const downloadLogFilePortionPromise = rds.downloadDBLogFilePortion(params).promise();
+    console.log('Downloading ' + DBInstanceIdentifier + ':' + data[data.length-2] +  '...')
     return downloadLogFilePortionPromise.then((data, err) => {
       return JSON.parse(JSON.stringify(data))['LogFileData'];
     });
